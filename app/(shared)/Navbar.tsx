623265396acb91d6bd2d43206eab99a4f2674cd0
Dmitriy Angve <kirirch129@gmail.com>
2023-05-22 16:34:15 +0300

    #

---
+++ app/(shared)/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <header className="mb-5">
+      <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4">
+        <div className="hidden sm:block">social link</div>
+        <div className="flex justify-between items-center gap-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Trending</Link>
+          <Link href="/">About</Link>
+        </div>
+        <div>
+          <p>Sign In</p>
+        </div>
+      </nav>
+      <div className="flex justify-center gap-8 mt-5 mb-4 mx-10">
+        {/* "basis-2/3" - width: 66% */}
+        <div className="basis-2/3 md:mt-3">
+          <h1 className="font-bold text-3xl md:text-5xl"></h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
+import Navbar from "app/(shared)/Navbar";
+// If I doing routes for Next JS, I can start "import" from "app/" because that's how they configured it
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
+// meta data for Searching systems
 export const metadata = {
   title: "Blog AI App",
   description: "Blog built in Bext JS that uses AI",
 };
+// meta data for Searching systems
 
 export default function RootLayout({
   children,
@@ -15,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html className={openSans.className} lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+        {/* <Footer /> */}
+      </body>
     </html>
   );
 }
