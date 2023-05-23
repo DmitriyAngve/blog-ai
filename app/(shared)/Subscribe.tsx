9f4fab2a2775bc49f7bf901b779d0dc600234df4
Dmitriy Angve <kirirch129@gmail.com>
2023-05-23 21:00:54 +0300

    #

---
+++ app/(shared)/Subscribe.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Subscribe = (props: Props) => {
+  return (
+    <div className="text-center bg-wh-10 px-5 py-10">
+      <h4 className="font-semibold text-base">Subscribe to our Newsletter</h4>
+    </div>
+  );
+};
+
+export default Subscribe;
