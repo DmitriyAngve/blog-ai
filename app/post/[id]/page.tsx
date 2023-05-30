72c979879ddb86ec0422d83151243ce8ec7c6c29
Dmitriy Angve <kirirch129@gmail.com>
2023-05-30 20:21:42 +0300

    #

---
+++ app/post/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { prisma } from "@/app/api/client";
+import React from "react";
+import { Post as PostType } from "@prisma/client";
+
+type Props = {
+  params: { id: string };
+};
+
+// grasb the relevant Post from Prisma in the right location
+const getPost = async (id: string) => {
+  const post: PostType | null = await prisma.post.findUnique({
+    where: { id },
+  });
+
+  // check if post doesn't exist
+  if (!post) {
+    console.log(`Post width id ${id} not found`);
+    return null;
+  }
+
+  // from get post we can't pass a date object which is something I getting from the backend and I cannot pass if into the component ("Post")
+  const formattedPost = {
+    ...post,
+    createdAt: post?.createdAt.toISOString(),
+    updatedAt: post?.updatedAt.toISOString(),
+  };
+
+  return formattedPost;
+};
+
+const Post = ({ params }: Props) => {
+  const { id } = params;
+  const post: FormattedPost | null = await getPost(id);
+  return (
+    <main className="px-10 leading-7">
+      <div className="md:flex gap-10 mb-5">
+        <div className="basis-3/4">
+          <Content />
+        </div>
+        <div className="basis-1/4">
+          <Sidebar />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Post;
