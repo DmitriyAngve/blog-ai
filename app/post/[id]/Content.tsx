f9ab4565bd49c2d07ce2c7d6c3634897993043b7
Dmitriy Angve <kirirch129@gmail.com>
2023-05-30 21:05:14 +0300

    #

--- app/post/[id]/Content.tsx
+++ app/post/[id]/Content.tsx
@@ -10,7 +10,26 @@ const Content = ({ post }: Props) => {
   const [title, seTitle] = useState<string>(post.title);
   const [titleError, setTitleError] = useState<string>("");
 
-  return <div>Content</div>;
+  const [content, setContent] = useState<string>(post.content);
+  const [contentError, setContentError] = useState<string>("");
+
+  return (
+    <div className="prose w-full max-w-full mb-10">
+      {/* BREADCRUMBS */}
+      <h5 className="text-wh-300">{`Home > ${post.category} > ${post.title}`}</h5>
+      {/* CATEGORY AND EDIT */}
+      <div className="flex justify-between items-center">
+        <h4 className="bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold">
+          {post.category}
+        </h4>
+        <div className="mt-4">
+            {isEditable ? (
+                <div></div>
+            )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
